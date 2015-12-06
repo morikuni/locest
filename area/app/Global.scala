6749f59a9fb65856385a84c4f59cfa80cee904e4
morikuni <taihei.morikuni@gmail.com>
2015-12-07 00:42:11 +0900

    ログの出力設定を追加

--- area/app/Global.scala
+++ area/app/Global.scala
@@ -1,16 +1,20 @@
 import com.github.morikuni.locest.area.application.dto.ErrorDto
-import play.api.GlobalSettings
-import play.api.http.ContentTypes
 import play.api.libs.json.Json
 import play.api.mvc.{RequestHeader, Result, Results}
+import play.api.{GlobalSettings, Logger}
 import scala.concurrent.Future
 import scala.util.control.NonFatal
 
 object Global extends GlobalSettings with Results {
+  val logger = Logger(getClass)
+
   override def onBadRequest(request: RequestHeader, error: String): Future[Result] = Future.successful(BadRequest(Json.toJson(ErrorDto("Bad request. Check API document."))))
 
-  override def onError(request: RequestHeader, ex: Throwable): Future[Result] = ex match {
-    case NonFatal(_) => Future.successful(InternalServerError(Json.toJson(ErrorDto.internalServerError)))
+  override def onError(request: RequestHeader, ex: Throwable): Future[Result] = {
+    logger.error("Unrecoverd error occured", ex)
+    ex match {
+      case NonFatal(_) => Future.successful(InternalServerError(Json.toJson(ErrorDto.internalServerError)))
+    }
   }
 
   override def onHandlerNotFound(request: RequestHeader): Future[Result] = Future.successful(NotFound(Json.toJson(ErrorDto("404: Not found."))))
